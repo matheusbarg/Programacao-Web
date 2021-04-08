040da6a7ba4000cbab65bbccb836e8b02af33310
matheusbarg <48385549+matheusbarg@users.noreply.github.com>
2021-04-07 23:03:23 -0300

    commit

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+
+app.get("/",(request,response) => {
+    console.log(request.query);
+    return response.send('Ola mundo');
+    });
+
+    app.get("/produto",(request,response) => {
+        return response.json({
+            id:1,
+            nome:'Jaqueta'
+        });
+    });
+
+    app.get("/produto",(request,response) => {
+       console.log(request.body);
+        return response.json({
+            id:1,
+            nome:'Jaqueta'
+        });
+    });
+
+    app.get("/produto/:id",(request,response) => {
+       console.log(request.params);
+        return response.json({
+            id:1,
+            nome:'Jaqueta'
+        });
+    });
+    
+
+app.get("/usuario",(request,response) => {
+    return response.json({
+           
+        id: 1,
+        name: "Matheus",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+          zipcode: "92998-3874",
+          geo: {
+            lat: "-37.3159",
+            lng: "81.1496"
+          }
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+          name: "Romaguera-Crona",
+          catchPhrase: "Multi-layered client-server neural-net",
+          bs: "harness real-time e-markets"
+        }
+
+        
+    
+    });
+    
+});
+
+
+app.listen(3334);
